a0cfc1c19e02b9a137baaf64a2868f1e8f99e475
zahidsom <157162870+zahidsom@users.noreply.github.com>
2024-01-26 13:14:01 +0500

    Update Items.tsx
    test for button.name and button.value

--- app/routes/Items.tsx
+++ app/routes/Items.tsx
@@ -106,3 +106,114 @@ const Items = useLoaderData <typeof loader>(); // Items = dataset(s), json, arra
       </main> 
   ); // return
 }
+import { db } from "~/db/config.server";
+import { ActionFunctionArgs, json} from "@remix-run/node";
+import { Form, Link, useLoaderData } from "@remix-run/react";
+import { items } from "~/db/schema.server";
+import { eq } from "drizzle-orm";
+
+          // backend  function 
+export async function loader() {
+          // use drizzle to get the dataset(s)
+  const dataSets =  db.select().from(items).all()
+  return json(dataSets); // array of key value pairs
+}
+
+          // backend  function 
+export async function action({
+  request,
+}: ActionFunctionArgs) {
+
+                // grab form fields values including button name & value
+  const formData = await request.formData();  
+  const {_action, ...values} = Object.fromEntries(formData);  
+
+                //    let {...values} = Object.fromEntries(formData);  
+
+                //  let title = String(formData.get("title"));
+                //  let desc = String(formData.get("description"));
+ 
+                //  console.log(" content of values:"  + JSON.stringify(values)  )
+  console.log("result:", values);
+                //  console.log("title:", values.title, "description:", values.description);
+              // console.log("action is :",_action)
+
+               // test for button.name and button.value
+   if (_action == "create")
+   {
+              // db.insert(items).values(
+              //                 { ...values }).run()
+
+              // export async function action() {
+              //   db.insert(items).values({ title: "Item title 12" }).run()
+              //   return {
+              //     success: true,
+              //   }
+              // }
+
+        db.insert(items).values({
+        title: values.title,
+        description: values.description,
+        createdAt: String(new Date().toLocaleDateString("en-GB",)),
+        updatedAt: String(new Date().toLocaleDateString("en-GB"))
+        }).run()
+        return {
+          success: true,
+      } 
+  }             
+
+          // test for button.name and button.value
+   if(_action == "delete")
+   {
+       return   await db.delete(items).where(eq(items.id, values.id));
+   }             
+
+   return {
+     success: true,
+   }
+}
+
+            // front end rendering
+export default function DisplayItems() {
+
+const Items = useLoaderData <typeof loader>(); // Items = dataset(s), json, array of key value pairs
+
+    return (
+      <main>
+          <h1>Items</h1>
+         {Items.length ? (
+            <ul>
+                { Items.map((item) => ( // map on Items, a json type, key value pair
+                <li key={item.id}>
+                   {item.id} {" "} {item.title} {item.description}
+
+                   <Form style={{display: "inline",}} method="post">
+                    <input type="hidden" name="id" value={item.id} />  {/* for use in delete,  where clause */}
+                              {/*   <input type="hidden" name="createAt" value={item.createdAt} /> */}
+                      <button
+                       type="submit"
+                       aria-label="delete"
+                       name="_action"
+                       value="delete"
+                       style={{margin: "0 0 0 1rem"}}>
+                        x
+                      </button>
+                   </Form>
+
+                </li>
+             ))}
+            </ul>
+           ) : (
+            <p> No Items</p>
+          )} 
+
+          <Form method="post" >
+            <input type="text" name="title" placeholder="Enter title" required /> {" "}
+            <input type="text" name="description" placeholder="Enter description" required /> {" "}
+            <button type="submit" name="_action" value="create">Add</button>
+          </Form>
+
+         <Link to="/">Home</Link>
+      </main> 
+  ); // return
+}
